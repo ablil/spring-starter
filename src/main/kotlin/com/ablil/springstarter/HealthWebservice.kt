acd4779c038491c05fd4a7d96632e967269fb2be
akhalil <25986426+ablil@users.noreply.github.com>
2023-09-03 16:48:32 +0100

    configure swaggger (#9)

--- src/main/kotlin/com/ablil/springstarter/HealthWebservice.kt
+++ src/main/kotlin/com/ablil/springstarter/HealthWebservice.kt
@@ -7,24 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@Tag(name = "Health", description = "Check application health")
+@Tag(name = "Health indicators")
 class HealthWebservice {
 
     // TODO: create test for all of these endpoints
 
     @GetMapping("/health")
-    @Operation(summary = "Check app health")
     fun health() = "health"
 
     @GetMapping("/private")
-    @Operation(summary = "Check app health with private endpoint")
     fun private() = "private"
 
     @GetMapping("/admin")
-    @Operation(summary = "Check app health for admin users")
     fun admin() = "admin"
 
     @GetMapping("/health/user")
-    @Operation(summary = "Get authenticated user username")
     fun getCurrentUser(): String = SecurityUtil.getCurrentUser().username
 }
