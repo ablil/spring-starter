d0cfc183067f1616290bf03531511f1a9d3cb241
ablil <ablil@protonmail.com>
2023-09-16 17:35:43 +0100

    add test for /health

---
+++ src/test/kotlin/com/ablil/springstarter/HealthWebserviceTest.kt
@@ -0,0 +1,20 @@
+package com.ablil.springstarter
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HealthWebserviceTest(
+    @Autowired val mockMvc: MockMvc,
+) {
+
+    @Test
+    fun `should be healthy`() {
+        mockMvc.get("/health").andExpect { status { isOk() } }
+    }
+}
