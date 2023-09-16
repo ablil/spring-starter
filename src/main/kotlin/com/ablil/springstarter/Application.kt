5b8b34a2ae6a8f1148eb89735618638f4619370b
akhalil <25986426+ablil@users.noreply.github.com>
2023-09-16 16:04:59 +0100

    fix swagger authorization (#13)

--- src/main/kotlin/com/ablil/springstarter/Application.kt
+++ src/main/kotlin/com/ablil/springstarter/Application.kt
@@ -3,17 +3,17 @@ package com.ablil.springstarter
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
 import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.security.SecurityScheme
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 @OpenAPIDefinition(
-    info = Info(title = "Spring boot API Starter", description = "API specification")
+    info = Info(title = "Spring boot API Starter", description = "API specification", version = "0.0.1"),
+    security = [SecurityRequirement(name = "Authorization")],
 )
-@SecurityScheme(
-    name = "Bearer token", type = SecuritySchemeType.HTTP, bearerFormat = "json web token", scheme = "bearer"
-)
+@SecurityScheme(name = "Authorization", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
 class Application
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/com/ablil/springstarter/miscllaneous/TestUser.kt
@@ -0,0 +1,34 @@
+package com.ablil.springstarter.miscllaneous
+
+import com.ablil.springstarter.domain.users.AccountStatus
+import com.ablil.springstarter.domain.users.User
+import com.ablil.springstarter.domain.users.UserRepository
+import jakarta.annotation.PostConstruct
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.security.crypto.password.PasswordEncoder
+
+
+@Configuration
+class TestUser(
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder
+) {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    @PostConstruct
+    @Profile("dev")
+    fun createTestUser() {
+        userRepository.save(
+            User(
+                id = null,
+                username = "joedoe",
+                email = "joedoe@example.com",
+                password = passwordEncoder.encode("supersecurepassword"),
+                status = AccountStatus.ACTIVE,
+                token = null
+            )
+        ).also { logger.info("Created test user username=joedoe, email=joedoe@example.com, password=supersecurepassword") }
+    }
+}
