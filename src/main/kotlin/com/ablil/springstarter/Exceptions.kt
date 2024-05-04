c17d4797be4d7e6e8c76a90ba29f90cec30c6a71
ablil <ablil@protonmail.com>
2024-05-04 11:51:44 -0400

    implement bearer token authentication with Keycloak

--- src/main/kotlin/com/ablil/springstarter/Exceptions.kt
+++ src/main/kotlin/com/ablil/springstarter/Exceptions.kt
@@ -9,10 +9,6 @@ abstract class DefaultBusinessError(
 ) : RuntimeException(message)
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-class TokenNotFound(s: String) :
-    DefaultBusinessError("auth-001", "token not found: $s")
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
 class ResourceNotFound(resource: String, id: String) : DefaultBusinessError(
     "business-001",
     "resource '$resource' with identifier '$id' not found",
@@ -23,14 +19,3 @@ class PageNotFound(page: Int) : DefaultBusinessError(
     "business-002",
     "page $page returned empty result",
 )
-
-@ResponseStatus(HttpStatus.CONFLICT)
-class UserAlreadyExists(
-    username: String,
-) : DefaultBusinessError("auth-002", "user $username already exists")
-
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
-class InvalidCredentials(override val message: String) : DefaultBusinessError("auth-003", message)
-
-@ResponseStatus(HttpStatus.FORBIDDEN)
-class ResetPasswordError(msg: String) : DefaultBusinessError("auth-004", msg)
